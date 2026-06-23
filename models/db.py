72a712d8801a5d124ac19553bdd8f8b5f4a7f2d0
JANANI N <jananinaveenkumar448@gmail.com>
2026-06-23 11:23:18 +0530

    day 14

--- models/db.py
+++ models/db.py
@@ -1,17 +1,58 @@
 import sqlite3
 
-def init_db():
-    conn = sqlite3.connect("students.db")
-    cursor = conn.cursor()
+DB = "students.db"
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        age INTEGER NOT NULL,
-        score REAL NOT NULL
-    )
+
+def get_connection():
+    return sqlite3.connect(DB)
+
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            age INTEGER,
+            score REAL
+        )
     """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_student(name, age, score):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO students (name, age, score)
+        VALUES (?, ?, ?)
+    """, (name, age, score))
+
+    conn.commit()
+    conn.close()
+
+
+def get_all_students():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+
+    conn.close()
+    return data
+
+
+def delete_student_by_id(student_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("DELETE FROM students WHERE id=?", (student_id,))
 
     conn.commit()
     conn.close()
